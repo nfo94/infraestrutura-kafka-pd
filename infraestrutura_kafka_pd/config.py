3bf66b21f319d88b6d1614be751a9d61597eac69
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2025-02-17 09:28:06 -0300

    chore: update config file

--- infraestrutura_kafka_pd/config.py
+++ infraestrutura_kafka_pd/config.py
@@ -1,5 +1,7 @@
 import os
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
-KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "kafka-infrastructure")
-KAFKA_CONSUMER_GROUP = os.getenv("KAFKA_CONSUMER_GROUP", "kafka-infrastructure-group")
+TOPIC_TRANSACTIONS = os.getenv("TOPIC_TRANSACTIONS", "transactions")
+TOPIC_FRAUDULENT_TRANSACTIONS = os.getenv(
+    "TOPIC_FRAUDULENT_TRANSACTIONS", "fraudulent_transactions"
+)
