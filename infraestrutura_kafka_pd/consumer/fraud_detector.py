e0eb1354dd7944eadc3afc9f6bec526d567f3f0e
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2025-02-17 21:44:47 -0300

    refactor: adjust names

---
+++ infraestrutura_kafka_pd/consumer/fraud_detector.py
@@ -0,0 +1,46 @@
+# Structure to save the context of the user
+user_context = {}
+
+
+async def detector(transaction):
+    user_id = transaction.user_id
+    if user_id not in user_context:
+        user_context[user_id] = {
+            "last_transaction": None,
+            "max_value": 0,
+            "last_country": None,
+        }
+
+    context = user_context[user_id]
+    fraud_type = None
+    fraud_params = {}
+
+    # Rule 1: high frequency
+    if (
+        context["last_transaction"]
+        and (transaction.timestamp - context["last_transaction"].timestamp) < 300
+    ):
+        fraud_type = "Alta Frequência"
+        fraud_params = {
+            "interval": transaction.timestamp - context["last_transaction"].timestamp
+        }
+
+    # Rule 2: high value
+    if transaction.value > 2 * context["max_value"]:
+        fraud_type = "High value"
+        fraud_params = {"value": transaction.value, "max_value": context["max_value"]}
+
+    # Rule 3: another country
+    if context["last_country"] and context["last_country"] != transaction.country:
+        fraud_type = "Another country"
+        fraud_params = {
+            "last_country": context["last_country"],
+            "current_country": transaction.country,
+        }
+
+    # Updates user context
+    context["last_transaction"] = transaction
+    context["max_value"] = max(context["max_value"], transaction.value)
+    context["last_country"] = transaction.country
+
+    return fraud_type, fraud_params
