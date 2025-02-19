d5559d30b961fb8ffd9cbea510fdb9f1e1b4fa1b
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2025-02-19 09:26:47 -0300

    chore: adjust names

--- infraestrutura_kafka_pd/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
-TOPIC_TRANSACTIONS = os.getenv("TOPIC_TRANSACTIONS", "transactions")
-TOPIC_FRAUDULENT_TRANSACTIONS = os.getenv(
-    "TOPIC_FRAUDULENT_TRANSACTIONS", "fraudulent_transactions"
-)
